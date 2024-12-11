172de1cfc12a44a4d152d1f0e86b9f58d2866c81
Karl Keller <kgk86@hotmail.com>
2024-12-11 22:18:09 +0100

    Remove unnecessary methods and specs

--- day1/hysteria.rb
+++ day1/hysteria.rb
@@ -1,19 +1,9 @@
-def get_smallest_number(list)
-  # get the smallest number from the list
-  # using #get_number_from()
-  get_number_from(1, list)
-end
-
 def get_left_list(location_id_lists)
   location_id_lists.map{|x,| x}
 end
 
 def get_right_list(location_id_lists)
   location_id_lists.map{|_, x| x}
-end
-
-def get_number_from(position, unsorted_list)
-  unsorted_list.sort[position - 1]
 end
 
 def sum_distances(sorted_left_list, sorted_right_list)
--- day1/spec/hysteria_spec.rb
+++ day1/spec/hysteria_spec.rb
@@ -14,15 +14,6 @@ sorted_left_list = left_list.sort
 sorted_right_list = right_list.sort
 
 describe "Historian Hysteria" do
-  describe "Get smallest numbers" do
-    it "returns the smallest number from the left list" do
-      expect(get_smallest_number(left_list)).to eq(1)
-    end
-    it "returns the smallest number from the right list" do
-      expect(get_smallest_number(right_list)).to eq(3)
-    end
-  end
-
   describe "Get left and right lists" do
     it "return the left list" do
       expect(get_left_list(example_list)).to eq([3,4,2,1,3,3])
@@ -30,34 +21,6 @@ describe "Historian Hysteria" do
 
     it "return the right list" do
       expect(get_right_list(example_list)).to eq([4,3,5,3,9,3])
-    end
-  end
-
-  describe "left list" do
-    it "return the smallest number" do
-      expect(get_number_from(1, left_list)).to eq(1)
-    end
-
-    it "return the second smallest number" do
-      expect(get_number_from(2, left_list)).to eq(2)
-    end
-  
-    it "return the third smallest number" do
-      expect(get_number_from(3, left_list)).to eq(3)
-    end
-  end
-
-  describe "right list" do
-    it "return the smallest number" do
-      expect(get_number_from(1, right_list)).to eq(3)
-    end
-
-    it "return the second smallest number" do
-      expect(get_number_from(2, right_list)).to eq(3)
-    end
-  
-    it "return the third smallest number" do
-      expect(get_number_from(3, right_list)).to eq(3)
     end
   end
 
