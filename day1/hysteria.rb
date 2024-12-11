d15800de5f88337ea0d2b54d5f52687be0e1d021
Karl Keller <kgk86@hotmail.com>
2024-12-11 21:56:34 +0100

    Add #get_total_distance() and #sum_distances()

--- day1/hysteria.rb
+++ day1/hysteria.rb
@@ -15,3 +15,15 @@ end
 def get_number_from(position, unsorted_list)
   unsorted_list.sort[position - 1]
 end
+
+def sum_distances(sorted_left_list, sorted_right_list)
+  sorted_left_list.zip(sorted_right_list).map do |pair|
+    (pair[0] - pair[1]).abs
+  end.sum
+end
+
+def get_total_distance(left_list, right_list)
+  sorted_left_list = left_list.sort
+  sorted_right_list = right_list.sort
+  sum_distances(sorted_left_list, sorted_right_list)
+end
