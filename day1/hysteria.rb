40aca37c6f0ae3b0d02b862d0536f337d72a2772
Karl Keller <kgk86@hotmail.com>
2024-12-11 21:19:17 +0100

    Commit day1 progress from yesterday
    Implemented methods:
    get_left_list()
    get_right_list()
    get_number_from()
    
    where a position of 1 is for getting the smallest number from a list,
    position of 2 for the second smallest number,
    position of 3 for third smallest, and so on...

--- day1/hysteria.rb
+++ day1/hysteria.rb
@@ -1,3 +1,15 @@
 def get_smallest_number(list)
   list.sort.first
 end
+
+def get_left_list(location_id_lists)
+  location_id_lists.map{|x,| x}
+end
+
+def get_right_list(location_id_lists)
+  location_id_lists.map{|_, x| x}
+end
+
+def get_number_from(position, sorted_list)
+  sorted_list.sort[position - 1]
+end
--- day1/spec/hysteria_spec.rb
+++ day1/spec/hysteria_spec.rb
@@ -1,17 +1,15 @@
 # frozen_string_literal: true
 require_relative "../hysteria.rb"
 
-list = <<~LIST
-  3   4
-  4   3
-  2   5
-  1   3
-  3   9
-  3   3
-LIST
+example_list = [[3, 4],
+                [4, 3],
+                [2, 5],
+                [1, 3],
+                [3, 9],
+                [3, 3]]
 
-left_list = [3, 4, 2, 1, 3, 3]
-right_list = [4, 3, 5, 3, 9, 3]
+left_list = get_left_list(example_list)
+right_list = get_right_list(example_list)
 
 describe "Historian Hysteria" do
   describe "Get smallest numbers" do
@@ -20,6 +18,61 @@ describe "Historian Hysteria" do
     end
     it "returns the smallest number from the right list" do
       expect(get_smallest_number(right_list)).to eq(3)
+    end
+  end
+
+  describe "Get left and right lists" do
+    it "return the left list" do
+      expect(get_left_list(example_list)).to eq([3,4,2,1,3,3])
+    end
+
+    it "return the right list" do
+      expect(get_right_list(example_list)).to eq([4,3,5,3,9,3])
+    end
+  end
+
+  describe "left list" do
+    it "return the smallest number" do
+      expect(get_number_from(1, left_list)).to eq(1)
+    end
+
+    it "return the second smallest number" do
+      expect(get_number_from(2, left_list)).to eq(2)
+    end
+  
+    it "return the third smallest number" do
+      expect(get_number_from(3, left_list)).to eq(3)
+    end
+  end
+
+  describe "right list" do
+    it "return the smallest number" do
+      expect(get_number_from(1, right_list)).to eq(3)
+    end
+
+    it "return the second smallest number" do
+      expect(get_number_from(2, right_list)).to eq(3)
+    end
+  
+    it "return the third smallest number" do
+      expect(get_number_from(3, right_list)).to eq(3)
+    end
+  end
+
+  describe "how far apart" do
+    it "return the distance between smallest numbers" do
+      skip
+      expect(distance(1, 3)).to eq(2)
+    end
+
+    it "return the distance between second smallest numbers" do
+      skip
+      expect(distance(2, 3)).to eq(1)
+    end
+
+    it "return the distance between third smallest numbers" do
+      skip
+      expect(distance(3, 3)).to eq(0)
     end
   end
 end
