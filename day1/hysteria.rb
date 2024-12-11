6b9801791559cc7bfa7387d3cc861154a64c7057
Karl Keller <kgk86@hotmail.com>
2024-12-11 21:23:38 +0100

    Refactor #get_smallest_number()

--- day1/hysteria.rb
+++ day1/hysteria.rb
@@ -1,5 +1,7 @@
 def get_smallest_number(list)
-  list.sort.first
+  # get the smallest number from the list
+  # using #get_number_from()
+  get_number_from(1, list)
 end
 
 def get_left_list(location_id_lists)
