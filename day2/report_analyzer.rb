7a8d710fa497376062bc8a7419e87c31e1642112
Karl Keller <kgk86@hotmail.com>
2024-12-14 23:50:14 +0100

    Day 2 - Refactor #is_safe?() and output count of safe reports

--- day2/report_analyzer.rb
+++ day2/report_analyzer.rb
@@ -2,34 +2,24 @@ def is_safe?(report)
   report_list = report.split(' ').map(&:to_i)
 
   # check if elements are all increasing or all decreasing
-  uniform_direction = false
-  increasing = increasing?(report_list, uniform_direction)
-  decreasing = decreasing?(report_list, uniform_direction)
-
   # is safe if uniform direction and difference within range
-  uniform_direction = increasing || decreasing
-  difference_within_range?(report_list) if uniform_direction
+  increasing = safe_levels?(report_list, '>')
+  decreasing = safe_levels?(report_list, '<')
+
+  increasing || decreasing
 end
 
-def increasing?(report_list, increasing)
-  report_list.each_cons(2) do |a,b|
-    increasing = b > a ? true : false
+def safe_levels?(report_list, operator)
+  report_list.each_cons(2).all? do |a,b|
+    b.send(operator.to_sym, a) && (b-a).abs <= 3
   end
-  increasing
 end
 
-def decreasing?(report_list, decreasing)
-  report_list.each_cons(2) do |a,b|
-    decreasing = b < a ? true : false
-  end
-  decreasing
-end
+input = File.readlines('input', chomp: true)
+puts "number of reports: #{input.size} reports"
 
-def difference_within_range?(list)
-  result = true
-
-  list.each_cons(2) do |a,b|
-    result = result && (b-a).abs <= 3
-  end
-  result
+safe_report_count = 0
+input.each do |report|
+  safe_report_count += 1 if is_safe?(report)
 end
+puts "number of safe reports is #{safe_report_count}"
